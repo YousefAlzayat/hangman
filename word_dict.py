6e760bfbfa70f7d4c03c8db739405278c82c810e
YousefAlzayat <67283820+YousefAlzayat@users.noreply.github.com>
2021-11-20 17:44:20 +0200

    Add files via upload

--- word_dict.py
+++ word_dict.py
@@ -1,3 +1,17 @@
+import os
+# Entering path of txt file to take words from such that it works as long as the file is in the same folder as the python code
+mainpath = os.path.dirname(os.path.abspath(__file__)) + "\\worddictionary.txt"
+mainpath = mainpath.split("\\")
+path = ""
+
+for i in mainpath:
+
+    if i != "worddictionary.txt":
+        path = path + i + "/"
+
+    else:
+        path = path + i
+
 word_list = []
-words = open("D:/Python/Hangman/worddictionary.txt", 'r')
+words = open(path, 'r')
 word_list = words.read().split("\n")
